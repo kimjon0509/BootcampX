4dc65320d734299a6f01edfa1af25605df301a3d
John Kim <kimjon0509@gmail.com>
2020-05-25 17:53:07 +0000

    created new assignment table and assignment_submission table with fake data

---
+++ migrations/assignments_submissions.sql
@@ -0,0 +1,18 @@
+-- Take a moment to try writing the CREATE TABLE statements yourself for assignments and assignment_submissions. Use the ERD to help.
+
+CREATE TABLE assignments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+);
+
+CREATE TABLE assignment_submissions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  duration INTEGER,
+  submission_date DATE
+);
